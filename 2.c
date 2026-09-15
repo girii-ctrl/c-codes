3a32bceadc3acfbc58bd3b3a3e873d37b297742a
Girish-3 <girishhh45@gmail.com>
2026-09-15 20:54:13 +0530

    add 2.c

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // the program to calculate area of a square 
+    int length ;
+    printf("enter the length of the square:");
+    scanf("%d",&length);
+    int area = length *length ;
+    printf("the area of the square is :%d\n",area);
+    return 0;
+
+}

3c279f35736738c6d968e636fc5404a5e702a844
Girish-3 <girishhh45@gmail.com>
2026-09-15 20:50:13 +0530

    add 1.c

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int age = 21 ;
+    float pi = 3.14 ;
+    char star = '*';
+    printf("the age is :%d\n",age);
+        printf("the pi value  is :%f\n",pi);
+    printf("the star symbol is :%c\n",star);
+return 0;
+}

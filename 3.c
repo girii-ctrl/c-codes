c3e17d1141ca0799201fe1a3b234dc78b615be6f
Girish-3 <girishhh45@gmail.com>
2026-09-16 20:14:07 +0530

    add 3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    // program to find the area of the square
+    float pi = 3.14 ;
+    int radius ;
+    printf("enter the radius of the cicrle:");
+    scanf("%d",&radius);
+     float area ;
+     area = pi * radius *radius ;
+printf("the area of the circle is :%f",area);
+return 0;
+
+}

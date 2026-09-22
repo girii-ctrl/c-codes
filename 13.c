324cbc1013f9193847befa49a0ab6b7554006015
Girish-3 <girishhh45@gmail.com>
2026-09-22 19:52:39 +0530

    add 13.c

---
+++ 13.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//print the numbers from 0 to 10 using for loop
+int main (){
+    printf("the numbers from 0 to 10 are");
+    for(int num = 0 ; num <= 10 ; num++ ){
+        printf("the numbers are :%d\n",num);
+    }return 0;
+}
